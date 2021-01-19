a09bdad77ccb35312066d29db9522a1b68d350e2
qidcoder <qidcoder@gmail.com>
2021-01-19 10:58:39 -0500

    step1 mod3

---
+++ stats/data.py
@@ -0,0 +1,10 @@
+import os
+import glob
+import pandas as pd
+
+game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
+#game_files now contains a list of all file names that end with .EVE in the games folder.
+
+# To better prepare the game_files list for reading into pandas, sort it in place by calling sort().
+
+# Note: There are two sorting functions in Python. To sort in place use list.sort(), not sorted(list) which returns a new list.
