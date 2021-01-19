bb9ae34ac0cb09878a6631dfcf758ff460117adb
qidcoder <qidcoder@gmail.com>
2021-01-19 11:26:36 -0500

    pt6

--- stats/data.py
+++ stats/data.py
@@ -16,4 +16,4 @@ game_frames = []
 for game_file in game_files:
     game_frame = pd.read_csv(game_file, names = ['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
 
-    game_frames += game_frame
+    game_frames.append(game_frame)
