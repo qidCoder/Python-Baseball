928fbaf20fb081f1047d77a61d9c405e2b72345a
qidcoder <qidcoder@gmail.com>
2021-01-19 11:19:42 -0500

    step5

--- stats/data.py
+++ stats/data.py
@@ -10,3 +10,6 @@ game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 game_files.sort()
 
 # Note: There are two sorting functions in Python. To sort in place use list.sort(), not sorted(list) which returns a new list.
+
+for each game_file in game_files:
+    game_frame = pd.read_csv(game_file, names = ['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
