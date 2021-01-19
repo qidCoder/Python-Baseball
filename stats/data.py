61b400879a4d003c1cc8c3e2665cc4fd6f8883ab
qidcoder <qidcoder@gmail.com>
2021-01-19 11:22:00 -0500

    5

--- stats/data.py
+++ stats/data.py
@@ -11,5 +11,5 @@ game_files.sort()
 
 # Note: There are two sorting functions in Python. To sort in place use list.sort(), not sorted(list) which returns a new list.
 
-for each game_file in game_files:
+for game_file in game_files:
     game_frame = pd.read_csv(game_file, names = ['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
