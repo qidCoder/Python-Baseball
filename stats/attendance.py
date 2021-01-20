de55899b8f73feb5229af96317e327478e93691c
qidcoder <qidcoder@gmail.com>
2021-01-20 16:36:23 -0500

    part 6

--- stats/attendance.py
+++ stats/attendance.py
@@ -7,3 +7,6 @@ from data import games
 attendance = games.loc[(games['multi2'] == 'attendance') & (games['type'] == 'info'), ['year', 'multi3']]
 
 attendance.columns = ['year', 'attendance']
+
+#convert to nummeric:
+attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
