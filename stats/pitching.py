9cc46881ef71391ebb88f9f6dd45562839ceee7e
qidcoder <qidcoder@gmail.com>
2021-01-20 16:52:43 -0500

    part 2

--- stats/pitching.py
+++ stats/pitching.py
@@ -2,4 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from data import games
 
-plays = games.loc[games['type'] == 'play', :]
+plays = games.[games['type'] == 'play']
+
+strike_outs = plays[plays['event'].str.contains('K')]
