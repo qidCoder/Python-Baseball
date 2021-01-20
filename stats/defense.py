d519ec75b05fd4beccf6af077f651ae12db978c6
qidcoder <qidcoder@gmail.com>
2021-01-20 18:02:04 -0500

    update

--- stats/defense.py
+++ stats/defense.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from frames import games, info, events
 
-plays = games.query('type' == 'play'&'event' != 'NP')
+plays = games.query("type == 'play'&event != 'NP'")
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
 pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
