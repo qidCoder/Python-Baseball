2b39252825a8ee761832b1857f082dee546595bc
qidcoder <qidcoder@gmail.com>
2021-01-20 17:01:46 -0500

    complete

--- stats/pitching.py
+++ stats/pitching.py
@@ -5,3 +5,17 @@ from data import games
 plays = games.[games['type'] == 'play']
 
 strike_outs = plays[plays['event'].str.contains('K')]
+
+#group by year and then by id
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+#.size() added a new column with the number of strikeouts in the game
+
+# convert groupy object to a dataframe
+strike_outs = strike_outs.reset_index(name='strike_outs')
+
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+
+#plot
+strike_outs.plot(x = 'year', y = 'strike_outs', kind= ' scatter').legend(['Strike Outs'])
+
+plt.show()
